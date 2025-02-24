e67b9d33f127da63aa0b5736c1570361a3e695a3
arjendeetman <arjendeetman@hotmail.com>
2025-02-24 10:44:37 +0100

    Update analysis files

--- src/analysis/extract_setupinfo.m
+++ src/analysis/extract_setupinfo.m
@@ -1,5 +1,9 @@
-% Requires readyaml from Maarten Jongeneel
-% https://www.mathworks.com/matlabcentral/fileexchange/136369-readyaml
+%{
+This file is part of 3D Sneak Peek. 3D Sneak Peek code is licensed under 
+the terms of GNU General Public License as published by the Free Software 
+Foundation. For more information and the LICENSE file, see 
+<https://github.com/3DCP-TUe/3DSneakPeek>.
+%}
 
 %% Clear and close
 close all; 
@@ -9,81 +13,30 @@ clc;
 %% Get file path
 path = mfilename('fullpath');
 [filepath, name, ext] = fileparts(path);
-
-
-%% Read session file
-
 cd(filepath);
-cd('../template/yyyymmdd_session/')
 
-files = dir('*.yml'); % Find all .yml files in the current directory
-file_paths = fullfile({files.folder}, {files.name}); % Combine folder and name
+%% Find components and copy pasta to data record (destination)
 
-session = readyaml(file_paths{1});
-
-%% Get all components including the folder path from the setupinfo lib
-
+% Folder path to setupinfo library
 cd(filepath);
 cd('../setupinfo/')
+library = pwd;
 
-root_folder = pwd;
-files = dir(fullfile(root_folder, '**', '*.yml'));
-
-n = length(files);
-names = cell(n, 1);
-folders = cell(n, 1);
-
-for i = 1:n
-    [~, name, ~] = fileparts(files(i).folder);
-    folders{i} = files(i).folder;
-    names{i} = name;
-end
-
-%% Get all components used in this session
-
-n = length(session.system_layout);
-components = cell(n, 1);
-
-for i = 1:n
-    components{i} = session.system_layout{i}.component;
-end
-
-%% List all paths of components used
-
-[found, indices] = ismember(components, names);
-
-if any(~found)
-    missing = components(~found);
-    error('The following components were not found: %s', strjoin(missing, ', '));
-end
-
-folders_used = folders(found);
-
+% Folder path to setupinfo of data record (destination)
 cd(filepath);
 cd('test/setupinfo')
 destination = pwd;
+
+% Read session metadata file
 cd(filepath);
+cd('../template/yyyymmdd_session/')
+session = readyaml(pwd + "\metadata.yml");
 
-if isfolder(destination)
-    rmdir(destination, 's');
-end
+% Get all components used in this session
+components = sneakpeek.get_session_components(session);
 
-mkdir(destination);
-
-% Loop through each folder path in your cell array
-for k = 1:length(folders_used)
-
-    source_folder = folders_used{k};  
-    [~, name, ~] = fileparts(source_folder);
-    destination_folder = fullfile(destination, name);
-
-    % Copy the folder and its entire contents recursively
-    [status, msg, ~] = copyfile(source_folder, destination_folder);
-
-    if ~status
-        error('Failed to copy folder "%s" to "%s": %s', srcFolder, destFolder, msg);
-    end
-end
+% Copy used components to destination folder
+sneakpeek.copy_setupinfo(library, destination, components, true);
 
 %% End
-disp('End of script')
+disp('End')
---
+++ src/analysis/sneakpeek.m
@@ -0,0 +1,95 @@
+%{
+This file is part of 3D Sneak Peek. 3D Sneak Peek code is licensed under 
+the terms of GNU General Public License as published by the Free Software 
+Foundation. For more information and the LICENSE file, see 
+<https://github.com/3DCP-TUe/3DSneakPeek>.
+%}
+
+classdef sneakpeek
+    methods(Static)
+
+        % -----------------------------------------------------------------
+        
+        % Gets all the components from a setupinfo folder (from a library 
+        % or data record)
+        function [names, folders] = get_setupinfo_components(root_folder)
+            files = dir(fullfile(root_folder, '**', '*.yml'));
+            n = length(files);
+            names = cell(n, 1);
+            folders = cell(n, 1);
+            
+            for i = 1:n
+                [~, name, ~] = fileparts(files(i).folder);
+                folders{i} = files(i).folder;
+                names{i} = name;
+            end
+        end
+
+        % -----------------------------------------------------------------
+
+        % Gets the components used in a session
+        function components = get_session_components(session)
+            n = length(session.system_layout);
+            components = cell(n, 1);
+            
+            for i = 1:n
+                components{i} = session.system_layout{i}.component;
+            end
+        end
+               
+        % -----------------------------------------------------------------
+
+        % Copies setupinfo from a library (root) to a record (destination 
+        % folder) for a given list of components
+        function copy_setupinfo(root, destination, components, ...
+                swipe_destination)
+            
+            % Get names and folders from root
+            [names, folders] = sneakpeek.get_setupinfo_components(root);
+
+            % Check if members exists
+            [found, ~] = ismember(components, names);
+            
+            % Check for missing members
+            if any(~found)
+                missing = components(~found);
+                error('The following components were not found: %s', ...
+                    strjoin(missing, ', '));
+            end
+            
+            % List the paths of hte component folders used
+            folders_used = folders(found);
+            
+            % Swipe directory
+            if swipe_destination == true
+                if isfolder(destination)
+                    rmdir(destination, 's');
+                end
+            end
+            
+            % Make directory
+            if isfolder(destination)
+                mkdir(destination);
+            end
+            
+            % Loop through each folder path in cell array
+            for k = 1:length(folders_used)
+            
+                source_folder = folders_used{k};  
+                [~, name, ~] = fileparts(source_folder);
+                destination_folder = fullfile(destination, name);
+            
+                % Copy the folder and its entire contents recursively
+                [status, msg, ~] = copyfile(source_folder, ...
+                    destination_folder);
+            
+                if ~status
+                    error('Failed to copy folder "%s" to "%s": %s', ...
+                        srcFolder, destFolder, msg);
+                end
+            end
+        end
+        
+        % -----------------------------------------------------------------
+    end
+end
